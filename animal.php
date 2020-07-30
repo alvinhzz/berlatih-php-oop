51490a9db9d55d02de9cda567c88b5850e2951c7
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2020-07-30 22:29:10 +0700

    edit dan menambahkan method pada tugas release 0

--- animal.php
+++ animal.php
@@ -2,27 +2,34 @@
 
 class Animal
 {
-    public $name, $legs, $coldBlooded;
+    public $name, $legs, $coldBlooded; //property
 
-    public function __construct($nama, $kaki, $berdarahDingin)
+    public function __construct($nama, $kaki, $berdarahDingin) //constructor method
     {
         $this->name = $nama;
         $this->legs = $kaki;
         $this->coldBlooded = $berdarahDingin;
     }
 
-    public function getName()
+    public function getName() //method for returning name data
     {
         return $this->name;
     }
 
-    public function getLegs()
+    public function getLegs() //method for returning legs data
     {
         return $this->legs;
     }
 
-    public function getColdBlooded()
+    public function getColdBlooded() //for returning blooded data
     {
         return $this->coldBlooded;
+    }
+
+    public function Animals()
+    {
+        $animals = "Nama hewan ini adalah $this->name, ia merupakan hewan berkaki $this->legs";
+
+        return $animals;
     }
 }
--- index.php
+++ index.php
@@ -2,8 +2,14 @@
 
 require "animal.php";
 
-$sheep = new Animal("shaun", 2, "false");
+$sheep = new Animal("shaun", 4, "false"); //object 1
+$ular = new Animal("Ular", 0, "True"); //object 2
 
 echo $sheep->getName() . "<br>";
 echo $sheep->getLegs() . "<br>";
 echo $sheep->getColdBlooded();
+
+echo "<br>";
+echo "<H3>HEWAN</H3>";
+echo $sheep->Animals() . "<br>";
+echo $ular->Animals();
